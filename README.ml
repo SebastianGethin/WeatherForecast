7e705a595693a1549ee285340183dc843aab8a55
Sebastian <144529350+SebastianGethin@users.noreply.github.com>
2023-09-09 11:41:03 +0100

    Create README.ml

---
+++ README.ml
@@ -0,0 +1,4 @@
+# Weather Forecast
+My solution for a data engineering challenge set by a coworker. The challenge was worded as follows;
+"Connect to the WeatherAPI apis, pull data, and do something with it".
+This was my interpretation of "do something with it".
